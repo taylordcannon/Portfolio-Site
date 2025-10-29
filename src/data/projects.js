5e1ec46887a08bf7dae8243b0cfb15819748278c
Taylor Cannon <taylor.d.cannon@gmail.com>
2025-10-29 13:13:13 -0500

    Fixing contact and projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,11 @@
+import IconMint from '@/assets/images/icon_mint.svg'
+import TrelloLogo from '@/assets/images/trello_clone_logo.svg'
+import TachesLogo from '@/assets/images/taches_logo.svg'
+
 export default [
     {
-        title: 'Developer Portfolio (Vue 3 + Vite + Tailwind)',
-        image: '/src/assets/images/icon_mint.svg',
+        title: 'Developer Portfolio',
+        image: IconMint,
         description:
             'A rebuilt version of my personal developer portfolio, now powered by Vue 3, Vite, and TailwindCSS for faster builds, modern design, and dynamic project management.',
         tech: 'Vue 3 • Vite • TailwindCSS',
@@ -9,7 +13,7 @@ export default [
     },
     {
         title: 'Trello Clone',
-        image: '/src/assets/images/trello_clone_logo.svg',
+        image: TrelloLogo,
         description:
             'A simple clone of the task-management website Trello using the React framework.',
         tech: 'HTML • CSS • React • Redux',
@@ -17,18 +21,10 @@ export default [
     },
     {
         title: 'Task Manager Web Application',
-        image: '/src/assets/images/taches_logo.svg',
+        image: TachesLogo,
         description:
             'Landing page for a future web app for task management, inspired by my experience building the Trello Clone.',
         tech: 'HTML • CSS',
         github: 'https://github.com/taylordcannon/tachesapp',
-    },
-    {
-        title: '2D Video Game',
-        image: '/src/assets/images/icon_rpg.svg',
-        description:
-            'The start of a 2D RPG video game that allows the user to control a character. Continued development planned for the future.',
-        tech: 'HTML • CSS • Webpack',
-        github: 'https://github.com/taylordcannon/rpg2d',
-    },
+    }
 ]
