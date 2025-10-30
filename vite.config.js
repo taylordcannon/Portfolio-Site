32617228f4ec1d7ab864cd8c86f81779e0f09cb4
Taylor Cannon <taylor.d.cannon@gmail.com>
2025-10-30 13:33:06 -0500

    Hopefully fix the refresh issue?

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import fs from 'fs'
 
 export default defineConfig({
-  base: '/',
-  plugins: [vue()],
+  base: '/', // or '/' if using custom domain
+  plugins: [
+    vue(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        fs.copyFileSync('dist/index.html', 'dist/404.html')
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
